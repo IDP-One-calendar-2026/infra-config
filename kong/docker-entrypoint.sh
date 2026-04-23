d90bc2d9fd8bac05c283e5884069e075ae195113
Manuel <manuelraileanu@gmail.com>
2026-04-24 00:49:37 +0300

    update the kong docker file to have permission to run

--- kong/docker-entrypoint.sh
+++ kong/docker-entrypoint.sh
@@ -4,7 +4,14 @@
 set -e
 
 # Substitute env vars into template
-envsubst < /usr/local/kong/declarative/kong.template.yaml > /usr/local/kong/declarative/kong.yaml
+KONG_CONFIG_PATH="${KONG_DECLARATIVE_CONFIG:-/tmp/kong.yaml}"
+
+if ! touch "$KONG_CONFIG_PATH" 2>/dev/null; then
+	KONG_CONFIG_PATH="/tmp/kong.yaml"
+fi
+
+envsubst < /usr/local/kong/declarative/kong.template.yaml > "$KONG_CONFIG_PATH"
+export KONG_DECLARATIVE_CONFIG="$KONG_CONFIG_PATH"
 
 # Execute the original Kong entrypoint with all arguments
 exec /entrypoint.sh "$@"
